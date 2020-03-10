d7c480a30b0308fc051935bb84ba01a5e89273e1
Ethan Lee <nathe.eel@gmail.com>
2020-03-10 12:59:06 -0400

    fixed applications function (#6)
    Co-authored-by: Ethan Lee <Ethan.Lee@ibm.com>

--- app/functions/applications.py
+++ app/functions/applications.py
@@ -1,6 +1,16 @@
 from app.common import getDb, buildResponse, checkCORS
 import json
 
+import decimal
+import datetime
+
+
+def alchemyencoder(obj):
+    if isinstance(obj, datetime.date):
+        return obj.isoformat()
+    elif isinstance(obj, decimal.Decimal):
+        return float(obj)
+
 
 def applications(request):
 
@@ -11,13 +21,15 @@ def applications(request):
     request_json = request.get_json()
     print(request_json)
 
+    bits = ["travel", "first_hack", "rating_diversity"]
+
     if request.method == 'POST':
         if "email" not in request_json:
             return buildResponse('Must contain an email', 400)
 
         with getDb().connect() as conn:
-            conn.execute("INSERT into applications VALUES (%s)",
-                         ",".join(request_json.values()))
+            conn.execute("INSERT into applications VALUES ({})".format(
+                ",".join(request_json[w] if w in bits else '"{0}"'.format(request_json[w]) for w in request_json.keys())))
 
         return buildResponse('', 200)
     elif request.method == 'GET':
@@ -25,7 +37,10 @@ def applications(request):
         with getDb().connect() as conn:
             res = conn.execute("select * from applications")
 
-        return json.dumps([dict(r) for r in res])
+        try:
+            return json.dumps([dict(r) for r in res], default=alchemyencoder)
+        except:
+            return buildResponse('Empty db', 400)
 
     elif request.method == 'PATCH':
         if "email" not in request_json:
